4b690f5662861533cecefdfad04b1b34c4b3c4f6
matos-nelson <nmatos6@gatech.edu>
2023-05-31 17:15:39 -0500

    Implemented getAll repository method and created mapper to convert list of entities to list of dtos.

--- src/main/java/org/rent/circle/address/api/persistence/repository/AddressRepository.java
+++ src/main/java/org/rent/circle/address/api/persistence/repository/AddressRepository.java
@@ -1,10 +1,16 @@
 package org.rent.circle.address.api.persistence.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
 import org.rent.circle.address.api.persistence.model.Address;
 
 @ApplicationScoped
 public class AddressRepository implements PanacheRepository<Address> {
 
+    public List<Address> getAll(List<Long> ids) {
+        Parameters queryParams = Parameters.with("ids", ids);
+        return list("id in :ids", queryParams);
+    }
 }
