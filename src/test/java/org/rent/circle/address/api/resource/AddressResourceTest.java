2c7553b2e210e6c35d914b3e2765914af204ea0e
matos-nelson <nmatos6@gatech.edu>
2023-05-31 18:22:54 -0500

    Implemented get all resource.

--- src/test/java/org/rent/circle/address/api/resource/AddressResourceTest.java
+++ src/test/java/org/rent/circle/address/api/resource/AddressResourceTest.java
@@ -2,12 +2,18 @@ package org.rent.circle.address.api.resource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import java.util.List;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+import org.rent.circle.address.dto.runtime.AddressDto;
 import org.rent.circle.address.dto.runtime.SaveAddressDto;
 
 @QuarkusTest
@@ -66,5 +72,43 @@ public class AddressResourceTest {
             .get("/100000")
             .then()
             .statusCode(204);
+    }
+
+    @Test
+    public void GetAll_WhenAddressesWithGivenIdsExist_ShouldReturnAddresses() {
+        // Arrange
+
+        // Act
+        List<AddressDto> result = given()
+            .when()
+            .get("/?ids=100")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .body()
+            .as(new TypeRef<List<AddressDto>>() {
+            });
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("4800 E Interstate 440 Road", result.get(0).getAddress1());
+        assertEquals("APT 1234", result.get(0).getAddress2());
+        assertEquals("90001", result.get(0).getZipcode());
+        assertEquals("Los Angeles", result.get(0).getCity());
+        assertEquals("CA", result.get(0).getStateCode());
+    }
+
+    @Test
+    public void GetAll_WhenAddressesWithGivenIdDoesNotExist_ShouldReturnOk() {
+        // Arrange
+
+        // Act
+        // Assert
+        given()
+            .when()
+            .get("?ids=100000")
+            .then()
+            .statusCode(200).body(is("[]"));
     }
 }
